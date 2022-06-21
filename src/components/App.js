d3992144b1137a4bbe7e3471ee57156822ee6c45
Surbhi Lande <81092552+surbhi322@users.noreply.github.com>
2022-06-21 23:27:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,84 +1,75 @@
-import React, { Component, useState } from "react";
-const initialState = {
-  input1: "",
-  input2: "",
-  text: ""
-};
-function relation(num) {
-  switch (num % 6) {
-    case 1:
-      return "Friends";
-    case 2:
-      return "Love";
-    case 3:
-      return "Affection";
-    case 4:
-      return "Marriage";
-    case 5:
-      return "Enemy";
-    case 0:
-      return "Siblings";
-    default:
-      return "Please Enter valid input";
-  }
+import React, { useState } from "react";
+import "./../styles/App.css";
+
+let globalId = 11;
+function App() {
+	const [inputVal,setInputVal] = useState();
+	const [todoItems,setTodoItems] = useState([]);
+	const [isEdit,setIsEdit] = useState(NaN);
+
+	const ele = todoItems.map((element)=>{
+		return <>
+			{!isNaN(isEdit) && element.id === isEdit ? (
+				<>
+					<textarea className="editTask" 
+					onChange={function(e){
+						setInputVal(e.target.value);
+					}}
+					value={inputVal==' ' ? element.task:inputVal}
+					></textarea>
+					<button className="saveTask" onClick={function(){
+						const newArray = todoItems.map((obj)=>{
+							if(obj.id===element.id){
+								obj.task = inputVal;
+							}
+							return obj;
+						})
+						setTodoItems(newArray);
+						setInputVal('');
+						setIsEdit(NaN);
+					}}>Save</button>
+				</>
+			):(
+				<>
+					<li className="list">{element.task}</li>
+			<button className="edit" onClick={function(){
+				setIsEdit(element.id);
+			}}>Edit</button>
+
+			<button className="delete" onClick={function(){
+				const newArrayList = todoItems.filter((item)=>{
+					return item.id !== element.id;
+				})
+				setTodoItems(newArrayList);
+			}}>Delete</button>
+				</>
+			)}
+			
+		</>
+	})
+	return (
+	<div id="main">
+	<form onSubmit={function(e){
+		e.preventDefault();
+		const obj = {
+			task:inputVal,
+			id:globalId++
+		}
+		setTodoItems([...todoItems,obj]);
+		setInputVal(' ');
+	}}>
+	<textarea id="task" 
+		value={inputVal}
+		onChange={function(e){
+		setInputVal(e.target.value);
+		}}>
+	</textarea>
+	<button id="btn" type="submit">Add Task</button>
+	</form>
+	<ol>{ele}</ol>
+	</div>
+	);
 }
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = initialState;
-    this.calculate = this.calculate.bind(this);
-    this.restart = this.restart.bind(this);
-  }
-  calculate = () => {
-    let str = this.state.input1.toLowerCase();
-    let str1 = this.state.input2.toLowerCase();
-    let result;
-    if (str.length > 0 && str1.length > 0) {
-      let count = 0;
-      for (let i of str) {
-        if (str1.indexOf(i) >= 0) {
-          count++;
-          i = 1;
-        }
-      }
-      const lenght = str.length + str1.length - count * 2;
-      result = relation(lenght);
-    } else {
-      result = relation(-1);
-    }
-    this.setState({ ...this.state, text: result });
-  };
-  restart = () => {
-    this.setState(initialState);
-  };
-  render() {
-    return (
-      <div id="main">
-        <h3 data-testid="answer">{this.state.text}</h3>
-        <input
-          type="text"
-          data-testid="input1"
-          value={this.state.input1}
-          onChange={(e) =>
-            this.setState({ ...this.state, input1: e.target.value })
-          }
-        />
-        <input
-          type="text"
-          data-testid="input2"
-          value={this.state.input2}
-          onChange={(e) =>
-            this.setState({ ...this.state, input2: e.target.value })
-          }
-        />
-        <button data-testid="calculate_relationship" onClick={this.calculate}>
-          Calculate Relationship Future
-        </button>
-        <button data-testid="clear" onClick={this.restart}>
-          Clear
-        </button>
-      </div>
-    );
-  }
-}
+
+
 export default App;
